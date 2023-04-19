c2432a95bdbaf5eae7825e20b0885ff78ae053e5
Vale <valeryabml@gmail.com>
2023-04-19 17:32:20 -0600

    Initial Commit

---
+++ ProyectoFinal1/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ProyectoFinal1
+//
+//  Created by Valeria Baeza on 19/04/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
