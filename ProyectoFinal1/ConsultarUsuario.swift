cff39ed6360a67128d23afbc6007a0f6d3ca06ba
Diego J <diegoajm2003@gmail.com>
2023-05-02 18:52:50 -0600

    YA FUNCIONA TABLEVIEW

---
+++ ProyectoFinal1/ConsultarUsuario.swift
@@ -0,0 +1,23 @@
+//
+//  ConsultarUsuario.swift
+//  ProyectoFinal1
+//
+//  Created by ISSC_412_2023 on 02/05/23.
+//
+
+import Cocoa
+
+class ConsultarUsuario: NSViewController {
+
+    @IBOutlet var vcTabla: ViewController!
+    @objc dynamic var usuarioLog:[UsuarioModelo] = []
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+    
+    
+}
